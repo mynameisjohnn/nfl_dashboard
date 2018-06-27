2913c908969a842f7da4e950742e590bc008f80f
Jayleh <jaylehyun@gmail.com>
2018-06-26 20:45:34 -0700

    adding 2015 to 2017 prediction data, nfl csv cleaning, machine learning edit

--- app.py
+++ app.py
@@ -23,5 +23,17 @@ def data():
     return jsonify(football_data)
 
 
+@app.route("/data-2015-2017")
+def data_2015_2017():
+
+    path = "data/nfl_2015_2017_prediction.csv"
+
+    df = pd.read_csv(path, encoding="utf-8")
+
+    football_data = df.to_dict(orient="records")
+
+    return jsonify(football_data)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
